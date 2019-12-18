89a08c93fbb7f362d28c7cdead97219a73b3ec33
paoloach <paolo.achdjian@skillbill.it>
2019-12-18 07:38:42 +0100

    Some fix

--- firmware/ESP8266/VASO2/main/Flash.c
+++ firmware/ESP8266/VASO2/main/Flash.c
@@ -2,32 +2,33 @@
 // Created by paolo on 13/12/19.
 //
 
-#include <string.h>
 #include <esp_log.h>
 #include <esp_err.h>
-#include <esp_spi_flash.h>
 #include <nvs_flash.h>
+#include <stdlib.h>
+#include <crc.h>
 #include "Flash.h"
-#include "taskSNTP.h"
 #include "Light.h"
 
-#define FULL_MAP 0x00
-#define MAX_SAMPLES 254
-#define BYTES_MAP 32
-
-extern struct PeriodLed periodLed;
+#define MAX_SAMPLES 100
+#define MAX_SECTORS 100
 
 struct DataBuffer {
-    uint8_t map[BYTES_MAP];
+    time_t firstTS;
+    time_t lastTS;
     struct DataSample samples[MAX_SAMPLES];
 } __attribute__((packed));
 
 static void readSector(uint16_t sector) ;
+static uint16_t getLastSectorFromFlash(time_t before);
 
 const char * TAG="FLASH";
 
 static struct DataBuffer dataBuffer;
 
+
+static const char *DATA_PARTITION_NAME="vaso2_data";
+static uint16_t lastSector;
 
 void initFlash(void) {
     esp_err_t espError;
@@ -37,6 +38,12 @@ void initFlash(void) {
     periodLed.end.hour = 21;
     periodLed.end.minute = 0;
 
+//    espError = nvs_flash_init_partition(DATA_PARTITION_NAME);
+//    if (espError != ESP_OK){
+//        ESP_LOGE(TAG, "error initializing data partition: %d", espError);
+//    }
+//
+//    lastSector = getLastSectorFromFlash(0);
 
     espError = nvs_flash_init();
     if (espError == ESP_OK){
@@ -68,62 +75,21 @@ struct DataSample *getSamples(uint16_t sector) {
     return dataBuffer.samples;
 }
 
-int16_t getLastSample() {
-    uint16_t sampleIndex = -1;
-    uint8_t *mapIter = dataBuffer.map;
-    uint8_t *mapIterEnd = dataBuffer.map + BYTES_MAP;
-    for (; mapIter != mapIterEnd; mapIter++) {
-        if (*mapIter != FULL_MAP) {
-            switch (*mapIter) {
-                case 0xFE:
-                    sampleIndex += 1;
-                    break;
-                case 0xFC:
-                    sampleIndex += 2;
-                    break;
-                case 0xF8:
-                    sampleIndex += 3;
-                    break;
-                case 0xF0:
-                    sampleIndex += 4;
-                    break;
-                case 0xE0:
-                    sampleIndex += 5;
-                    break;
-                case 0xC0:
-                    sampleIndex += 6;
-                    break;
-                case 0x80:
-                    sampleIndex += 7;
-                    break;
-                case 0xFF:
-                    break;
-                default:
-                    ESP_LOGE(TAG, "invalid map value %d at index %d", *mapIter,(uint16_t )(mapIter - dataBuffer.map));
-            }
-            return sampleIndex;
-        }
-        sampleIndex += 8;
-    }
-    return BYTES_MAP * 8 - 1;
+uint16_t getLastSector(time_t before) {
+    return 0;
 }
 
 
-uint16_t getLastSector(time_t before) {
+uint16_t getLastSectorFromFlash(time_t before) {
     time_t newerTime = 0;
     uint16_t newerSector = 0;
-    for (uint16_t sector = 0xF0; sector <= 0xFF; sector++) {
+    for(int sector=0; sector < MAX_SECTORS; sector++){
         readSector(sector);
-        uint16_t sampleIndex = getLastSample();
-        if (sampleIndex!=0xFFFF) {
-            time_t lastTime = dataBuffer.samples[sampleIndex].timestamp;
-            ESP_LOGI(TAG, "sample index: %d, time %ld\n", sampleIndex, lastTime);
-            if (lastTime > newerTime && lastTime <= before) {
-                newerTime = lastTime;
-                newerSector = sector;
-            }
-        } else {
-            ESP_LOGI(TAG,"sector %d is empty\n", sector);
+        time_t lastTime = dataBuffer.lastTS;
+        ESP_LOGI(TAG, "sample index: time %ld\n", dataBuffer.lastTS);
+        if (dataBuffer.lastTS > newerTime && lastTime <= before) {
+            newerTime = dataBuffer.lastTS;
+            newerSector = sector;
         }
     }
     return newerSector;
@@ -131,10 +97,16 @@ uint16_t getLastSector(time_t before) {
 
 static void readSector(uint16_t sector) {
     ESP_LOGI(TAG, "Read sector %02X\n", sector);
-    esp_err_t error =
-            spi_flash_read((uint32_t)sector * 0x1000, (uint32_t *)&dataBuffer, sizeof(struct DataBuffer));
+    nvs_handle nvsHandle;
+    esp_err_t  error = nvs_open_from_partition(DATA_PARTITION_NAME, "data",  NVS_READWRITE,&nvsHandle );
+    if (error != ESP_OK){
+        ESP_LOGE(TAG, "error open data partition: %d", error );
+    }
+    char sectorName[8];
+    itoa(sector,sectorName,10 );
+    error = nvs_get_blob(nvsHandle, sectorName, (uint32_t *)&dataBuffer, sizeof(struct DataBuffer));
     if (error != ESP_OK) {
         ESP_LOGE(TAG, "error reading sector %d: %d\n", sector, error);
-        memset(&dataBuffer.map, 0xFF, BYTES_MAP);
     }
+    nvs_close(nvsHandle);
 }
