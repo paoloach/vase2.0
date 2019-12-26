c12270590099fa2f0df6245ca58e2eb59315ca6f
paoloach <paolo.achdjian@skillbill.it>
2019-12-26 07:56:54 +0100

    Add freecad files

--- Mobile/Vase2.0/app/src/main/java/it/achdjian/paolo/vase20/Rest/Off.kt
+++
@@ -1,8 +0,0 @@
-package it.achdjian.paolo.vase20.Rest
-
-/**
- * Created by Paolo Achdjian on 2/10/19.
- */
-class Off(val restEngine: RestEngine): Runnable {
-    override fun run() =  restEngine.postMethod("/off")
-}
